5736b2dc93abf1c02d73b1ddc00824c3a7ad4b7f
Amilthan Madan <amilthan@Amilthans-MacBook-Pro.local>
2025-04-04 09:30:34 -0400

    Skills,timeline pages using D3.js

--- js/skills.js
+++ js/skills.js
@@ -1,6 +1,48 @@
 // js/skills.js
 d3.csv("data/dashboard_cleaned_data.csv").then(data => {
-    console.log("Skills data loaded:", data);
-    // Add D3 skill chart code here
+    const width = 700;
+    const height = 500;
+    const margin = { top: 20, right: 20, bottom: 60, left: 150 };
+  
+    const svg = d3.select("#skillsChart")
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height);
+  
+    const skillCounts = d3.rollup(data, v => v.length, d => d.skill);
+    const topSkills = Array.from(skillCounts, ([skill, count]) => ({ skill, count }))
+      .sort((a, b) => d3.descending(a.count, b.count))
+      .slice(0, 10);
+  
+    const x = d3.scaleLinear()
+      .domain([0, d3.max(topSkills, d => d.count)])
+      .range([margin.left, width - margin.right]);
+  
+    const y = d3.scaleBand()
+      .domain(topSkills.map(d => d.skill))
+      .range([margin.top, height - margin.bottom])
+      .padding(0.1);
+  
+    svg.append("g")
+      .selectAll("rect")
+      .data(topSkills)
+      .join("rect")
+      .attr("x", x(0))
+      .attr("y", d => y(d.skill))
+      .attr("width", d => x(d.count) - x(0))
+      .attr("height", y.bandwidth())
+      .attr("fill", "steelblue");
+  
+    svg.append("g")
+      .attr("transform", `translate(0,${margin.top})`)
+      .call(d3.axisTop(x).ticks(5))
+      .selectAll("text")
+      .style("font-size", "12px");
+  
+    svg.append("g")
+      .attr("transform", `translate(${margin.left},0)`)
+      .call(d3.axisLeft(y))
+      .selectAll("text")
+      .style("font-size", "12px");
   });
   
--- js/timeline.js
+++ js/timeline.js
@@ -1,5 +1,53 @@
 // js/timeline.js
 d3.csv("data/dashboard_cleaned_data.csv").then(data => {
-    console.log("Timeline data loaded:", data);
-    // Add D3 timeline chart code here
+    const parseDate = d3.timeParse("%Y-%m-%d");
+    data.forEach(d => {
+      d.date = parseDate(d.date.slice(0, 10)); // Handle timestamp format
+    });
+  
+    const jobCounts = d3.rollups(data, v => v.length, d => d.date)
+      .sort((a, b) => d3.ascending(a[0], b[0]))
+      .map(([date, count]) => ({ date, count }));
+  
+    const width = 800;
+    const height = 400;
+    const margin = { top: 20, right: 30, bottom: 50, left: 60 };
+  
+    const svg = d3.select("#timelineChart")
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height);
+  
+    const x = d3.scaleTime()
+      .domain(d3.extent(jobCounts, d => d.date))
+      .range([margin.left, width - margin.right]);
+  
+    const y = d3.scaleLinear()
+      .domain([0, d3.max(jobCounts, d => d.count)])
+      .nice()
+      .range([height - margin.bottom, margin.top]);
+  
+    const line = d3.line()
+      .x(d => x(d.date))
+      .y(d => y(d.count));
+  
+    svg.append("path")
+      .datum(jobCounts)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 2)
+      .attr("d", line);
+  
+    svg.append("g")
+      .attr("transform", `translate(0,${height - margin.bottom})`)
+      .call(d3.axisBottom(x).ticks(6))
+      .selectAll("text")
+      .style("font-size", "12px");
+  
+    svg.append("g")
+      .attr("transform", `translate(${margin.left},0)`)
+      .call(d3.axisLeft(y))
+      .selectAll("text")
+      .style("font-size", "12px");
   });
+  
